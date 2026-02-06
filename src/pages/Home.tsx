7c13af91b58d9ccd4e1828e2cfa603ea37b7b765
SaculAvlis <40922704+SaculAvlis@users.noreply.github.com>
2026-02-06 14:41:57 +0000

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -175,7 +175,7 @@ function Home() {
 
             <div className="glass-panel p-3 h-full flex flex-col">
               <div className="relative rounded-xl overflow-hidden min-h-[280px]">
-                <img src="/working-photo.jpg" alt="Mapping the Blue Orbit vision" className="absolute inset-0 w-full h-full object-cover" />
+                <img src="/working-photo.jpg" alt="Mapping the Blue Orbit vision" className="absolute inset-0 w-full h-full object-contain" />
                 <div className="absolute inset-0 bg-gradient-to-t from-deep/60 via-transparent to-transparent" />
               </div>
               <div className="p-4">
